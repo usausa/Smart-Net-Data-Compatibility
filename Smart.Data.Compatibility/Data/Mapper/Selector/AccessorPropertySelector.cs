97966a149513d3e223dc01978677eae6f0bc8f1a
usausa <machi.pon@gmail.com>
2025-12-07 13:22:30 +0900

    Update for improvements

--- Smart.Data.Compatibility/Data/Mapper/Selector/AccessorPropertySelector.cs
+++ Smart.Data.Compatibility/Data/Mapper/Selector/AccessorPropertySelector.cs
@@ -1,7 +1,6 @@
 namespace Smart.Data.Mapper.Selector;
 
 using System;
-using System.Linq;
 using System.Reflection;
 
 using Smart.Data.Accessor.Attributes;
@@ -17,30 +16,39 @@ public sealed class AccessorPropertySelector : IPropertySelector
 
     public PropertyInfo? SelectProperty(PropertyInfo[] properties, string name)
     {
-        var pi = properties.FirstOrDefault(x => IsMatchName(x, name, false)) ??
-                 properties.FirstOrDefault(x => IsMatchName(x, name, true));
-        if (pi is not null)
-        {
-            return pi;
-        }
+        PropertyInfo? caseInsensitiveMatch = null;
+        PropertyInfo? pascalMatch = null;
+        PropertyInfo? pascalCaseInsensitiveMatch = null;
+        string? pascalName = null;
 
-        var pascalName = Inflector.Pascalize(name);
-        if (pascalName != name)
+        foreach (var pi in properties)
         {
-            pi = properties.FirstOrDefault(x => IsMatchName(x, pascalName, false)) ??
-                 properties.FirstOrDefault(x => IsMatchName(x, pascalName, true));
-            if (pi is not null)
+            var propertyName = pi.GetCustomAttribute<NameAttribute>()?.Name ?? pi.Name;
+            if (String.Equals(propertyName, name, StringComparison.Ordinal))
             {
                 return pi;
             }
+
+            if ((caseInsensitiveMatch is null) && String.Equals(propertyName, name, StringComparison.OrdinalIgnoreCase))
+            {
+                caseInsensitiveMatch = pi;
+            }
+
+            pascalName ??= Inflector.Pascalize(name);
+            if (pascalName != name)
+            {
+                if ((pascalMatch is null) && String.Equals(propertyName, pascalName, StringComparison.Ordinal))
+                {
+                    pascalMatch = pi;
+                }
+
+                if ((pascalCaseInsensitiveMatch is null) && String.Equals(propertyName, pascalName, StringComparison.OrdinalIgnoreCase))
+                {
+                    pascalCaseInsensitiveMatch = pi;
+                }
+            }
         }
 
-        return null;
-    }
-
-    private static bool IsMatchName(PropertyInfo pi, string name, bool ignoreCase)
-    {
-        var propertyName = pi.GetCustomAttribute<NameAttribute>()?.Name ?? pi.Name;
-        return String.Equals(propertyName, name, ignoreCase ? StringComparison.OrdinalIgnoreCase : StringComparison.Ordinal);
+        return caseInsensitiveMatch ?? pascalMatch ?? pascalCaseInsensitiveMatch;
     }
 }
